69aa2943fdef96b35bdedfe178001f2323bf0195
NataR 78 <vip.aleksa78@mail.ru>
2022-09-21 20:45:39 +0300

    drop-down-php-ajax

---
+++ script.js
@@ -0,0 +1,71 @@
+$.ajax({
+    type: "POST",
+    url: "test.php",
+    data: {
+        query: "get_continents",
+    },
+    success: function(response) {
+        console.log(response)
+        let jsonData = JSON.parse(response)
+
+        $.each(jsonData, function(indexInArray, valueOfElement) {
+
+            let htmlBlock = `
+                <option class="optionSelect">` + valueOfElement["continent"] + `</option>
+             `
+
+            $("#continents").append(htmlBlock)
+        });
+    }
+});
+
+$(function() {
+    $('#continents').change(function() {
+        let thisContinent = $(this).val()
+
+        $('.table_country').empty()
+        $.ajax({
+            type: "POST",
+            url: "test.php",
+            data: {
+                query: "get_country",
+                continent: thisContinent,
+            },
+            success: function(response) {
+              
+                let jsonData = JSON.parse(response)
+
+                $.each(jsonData, function(indexInArray, valueOfElement) {
+
+                    let htmlBlock = `
+                    <p class="tb">` + valueOfElement["name"]+"-"+valueOfElement["population"] + `</p>
+                    `
+                    $(".table_country").append(htmlBlock)
+                });
+            }
+        });
+
+    });
+});
+
+$("#population").on("input",function(){
+    let thisPopulation = $(this).val()
+    alert($(this).val())
+    $.ajax({
+        type: "POST",
+        url: "test.php",
+        data: {
+            query: "get_population",
+            continent: thisPopulation ,
+        },
+        success: function(response) {
+          
+            let jsonData = JSON.parse(response)
+
+            $.each(jsonData, function(indexInArray, valueOfElement) {
+
+            });
+        }
+    });
+
+})
